7748241c5bd9524a984f2ffb72a93b2ec1dc316b
Илона Теблоева <tebloeva.ilona@bk.com>
2020-04-21 18:59:49 +0300

    Добавьте файлы проекта.

---
+++ inf6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace inf6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 5, 4, 8, 2 };
+            Stack<int> st = new Stack<int>();
+
+            foreach (var el in a)
+                st.Push(el);
+
+            for (; st.Count() > 0;)
+                Console.Write(st.Pop());
+        }
+    }
+}
